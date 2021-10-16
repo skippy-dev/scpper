1490df2e77a030eedd0d28c477c9b71e0af27ba3
mrnereof <mrnereof@gmail.com>
2021-10-16 19:02:45 +0500

    v1.1.4

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
 import setuptools
 import scpper
 
-with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
-with open('README.md', encoding="utf8") as f:
+with open("README.md", encoding="utf8") as f:
     readme = f.read()
 
 setuptools.setup(
-    name='scpper',
+    name="scpper",
     version=scpper.__version__,
-    description='Python wrapper for ScpperDB API.',
+    description="Python wrapper for ScpperDB API.",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url='https://github.com/skippy-dev/scpper/',
-    author='MrNereof',
-    author_email='mrnereof@gmail.com',
-    python_requires='>=3.8',
-    license='MIT',
+    url="https://github.com/skippy-dev/scpper/",
+    author="MrNereof",
+    author_email="mrnereof@gmail.com",
+    python_requires=">=3.8",
+    license="MIT",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"],
-    keywords=['scp', 'scpper', 'api'],
-    packages=['scpper'],
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords=["scp", "scpper", "api"],
+    packages=["scpper"],
     install_requires=requirements,
 )
